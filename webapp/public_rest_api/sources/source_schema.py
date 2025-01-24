018071bdb923ccd1c2fd465249c6456921a47eea
Ernesto Ruge <ernesto.ruge@binary-butterfly.de>
2025-01-24 09:49:07 +0100

    add fields to doc

--- webapp/public_rest_api/sources/source_schema.py
+++ webapp/public_rest_api/sources/source_schema.py
@@ -3,7 +3,9 @@ Copyright 2024 binary butterfly GmbH
 Use of this source code is governed by an MIT-style license that can be found in the LICENSE.txt.
 """
 
-from flask_openapi.schema import DateTimeField, IntegerField, JsonSchema, StringField, UriField
+from flask_openapi.schema import DateTimeField, EnumField, IntegerField, JsonSchema, StringField, UriField
+
+from webapp.models.source import SourceStatus
 
 source_schema = JsonSchema(
     title='Source',
@@ -18,6 +20,12 @@ source_schema = JsonSchema(
         'attribution_license': StringField(required=False),
         'attribution_contributor': StringField(maxLength=256, required=False),
         'attribution_url': StringField(maxLength=256, required=False),
+        'static_data_updated_at': DateTimeField(required=False),
+        'realtime_data_updated_at': DateTimeField(required=False),
+        'static_status': EnumField(enum=SourceStatus, required=False),
+        'realtime_status': EnumField(enum=SourceStatus, required=False),
+        'static_parking_site_error_count': IntegerField(required=False),
+        'realtime_parking_site_error_count': IntegerField(required=False),
     },
 )
 
